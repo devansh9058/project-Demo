eef67713ce897e9da1a7039653560a7fe8a675e2
Devansh Gangwar <gangwardevansh712@gmail.com>
2025-07-24 11:17:29 +0530

    all file

--- src/main/java/com/example/Authentication/Entity/User.java
+++ src/main/java/com/example/Authentication/Entity/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,7 +15,7 @@ import java.util.List;
 @Data
 @Entity
 @Table(name="user")
-
+@NoArgsConstructor
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
